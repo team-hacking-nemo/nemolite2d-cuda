825080b124c328315195833cd05356e68e774c9f
Joseph Thacker <joseph.thacker@stfc.ac.uk>
2019-08-19 11:16:09 +0100

    Changed the precision in the kinds
    in the finite difference I have changed the precision
    from real kind (double) to c_iso_binding c_float

--- shared/dl_esm_inf/finite_difference/src/kind_params_mod.f90
+++ shared/dl_esm_inf/finite_difference/src/kind_params_mod.f90
@@ -1,10 +1,11 @@
 !> Module holding basic KIND parameters
 MODULE kind_params_mod
+  use, intrinsic::iso_c_binding, only : c_float, c_double
   IMPLICIT none
 
   PUBLIC
 
   !> Douple precision kind parameter
-  INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307)
+  INTEGER, PARAMETER :: wp = c_float
 
 END MODULE kind_params_mod
